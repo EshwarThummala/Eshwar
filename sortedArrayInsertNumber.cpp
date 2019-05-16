49ffdfd5868cb0c2d80b54ffd46705c51338bf6a
EshwarThummala <eshwarreddy.thummala@gmail.com>
2019-05-16 23:30:15 +0530

    CArraysLesson2_sortedArrayInsertNumber

---
+++ sortedArrayInsertNumber.cpp
@@ -0,0 +1,57 @@
+/*
+OVERVIEW: Given a sorted array, insert a given number into the array at appropriate position.
+			E.g.: Arr = [2, 4, 6], num = 5 final Arr = [2, 4, 5, 6]. 5 is inserted at 2nd index.
+
+INPUTS: Integer	Array pointer, length of the array, number to be inserted.
+
+OUTPUT: Array pointer after inserting number.
+
+ERROR CASES: Return NULL for invalid inputs.
+
+NOTES: Use memory allocation techniques to allocate memory.
+
+ProblemCode :SAIN
+*/
+
+#include <stdio.h>
+#include <malloc.h>
+
+int * sortedArrayInsertNumber(int *Arr, int len, int num)
+{
+     if(len<=0)
+        return NULL;
+     if(Arr==NULL)
+        return NULL;
+ int temp,temp1,index,index1;
+ Arr=(int*)realloc(Arr,1*sizeof(int));
+ if(num<Arr[0])
+ {
+   temp=Arr[0];
+   Arr[0]=num;
+   for(index=1;index<len+1;index++)
+   {
+       temp1=Arr[index];
+       Arr[index]=temp;
+       temp=temp1;
+   }
+ }
+ if(num>Arr[len-1])
+ {
+     Arr[len]=num;
+ }
+for(index=0;index<len;index++)
+{
+    if(num>Arr[index]&&num<Arr[index+1])
+    {temp=Arr[index+1];
+      Arr[index+1]=num;
+      for(index1=index+2;index1<len+1;index1++)
+      {
+       temp1=Arr[index1];
+       Arr[index1]=temp;
+       temp=temp1;
+      }
+      break;
+    }
+}
+return Arr;
+}
